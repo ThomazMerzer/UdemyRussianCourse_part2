3f230a487af6e0deabef3160c139418ce7e552f8
thomazmerzer <thomazmerzer@gmail.com>
2019-10-01 00:22:46 +0500

    add: rotation and scale animation to pictures

--- app/src/main/java/com/me/udemyrussiacourse_part2/MainActivity.kt
+++ app/src/main/java/com/me/udemyrussiacourse_part2/MainActivity.kt
@@ -2,11 +2,38 @@ package com.me.udemyrussiacourse_part2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var isHiden = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        title = "Tom and Jerry"
+    }
+
+    fun eraseTom(view: View) {
+        when(isHiden) {
+            false -> {
+                returnToDefault()
+                img_center1.animate().rotation(360f*3).scaleY(0.1f).scaleX(0.1f).alpha(0f).duration = 1000
+                img_center2.animate().rotation(360f*3).scaleY(1f).scaleX(1f).alpha(1f).duration = 1000
+                isHiden = true
+            }
+            else -> {
+                returnToDefault()
+                img_center2.animate().rotation(360f*3).scaleY(0.1f).scaleX(0.1f).alpha(0f).duration = 1000
+                img_center1.animate().rotation(360f*3).scaleY(1f).scaleX(1f).alpha(1f).duration = 1000
+                isHiden = false
+            }
+        }
+    }
+    fun returnToDefault() {
+        img_center1.rotation = 0f
+        img_center2.rotation = 0f
     }
 }
