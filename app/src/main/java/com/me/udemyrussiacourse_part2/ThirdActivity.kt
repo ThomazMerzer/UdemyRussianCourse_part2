4ed609225e1f2df3f1a59205ba0b3a3b471490c5
thomazmerzer <thomazmerzer@gmail.com>
2019-10-05 23:20:31 +0300

    add: AudioManager with SeekBar

--- app/src/main/java/com/me/udemyrussiacourse_part2/ThirdActivity.kt
+++ app/src/main/java/com/me/udemyrussiacourse_part2/ThirdActivity.kt
@@ -1,37 +1,75 @@
 package com.me.udemyrussiacourse_part2
 
+import android.content.Context
+import android.media.AudioManager
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.SeekBar
 import kotlinx.android.synthetic.main.activity_third.*
 
 class ThirdActivity : AppCompatActivity() {
 
     lateinit var mediaPlayer: MediaPlayer
+    lateinit var audioManager: AudioManager
+    var maxVolume: Int = 0
+    var btnText = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third)
 
         title = "Audio"
+
+        prepareSeekBar()
+    }
+
+    fun prepareSeekBar() {
+        //Ставим контекст и путь до файла
         mediaPlayer = MediaPlayer.create(baseContext, R.raw.stuff)
+        //Получаем объект audio manager с приведением типа
+        audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        //Получаем максимальную грокость на канале
+        maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
+        //Назначаем максимальное число для seek bar
+        skbrVolume.max = maxVolume
+        //Назначаем положение ползунка при запуске активити
+        skbrVolume.progress = 0
+        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, skbrVolume.progress, 0)
+
+        skbrVolume.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, b: Boolean) {
+                txvVolume.text = "$progress"
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+
+            }
+        })
     }
 
     fun playPauseMusic(v: View) {
-        when (mediaPlayer.isPlaying) {
+        btnText = when (mediaPlayer.isPlaying) {
             true -> {
                 mediaPlayer.pause()
-                btnPlayPause.text = "Play"
+                "Play"
             }
             else -> {
                 mediaPlayer.start()
-                btnPlayPause.text = "Pause"
+                "Pause"
             }
         }
+        btnPlayPause.text = btnText
     }
     fun stopMusic(v: View) {
         mediaPlayer.stop()
+        btnPlayPause.text = "Play"
         mediaPlayer = MediaPlayer.create(baseContext, R.raw.stuff)
     }
 }
