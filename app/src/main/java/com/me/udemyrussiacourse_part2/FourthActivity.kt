58c2c1d0bea46e1d15c1ebb5678c2f15c5fce25d
thomazmerzer <thomazmerzer@gmail.com>
2019-10-06 22:40:11 +0300

    add: prepare activity for next lesson

--- app/src/main/java/com/me/udemyrussiacourse_part2/FourthActivity.kt
+++ app/src/main/java/com/me/udemyrussiacourse_part2/FourthActivity.kt
@@ -2,13 +2,20 @@ package com.me.udemyrussiacourse_part2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.widget.Toolbar
 
 class FourthActivity : AppCompatActivity() {
+
+    lateinit var musicToolBar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fourth)
 
-        title = "Music Player"
+        //set custom toolbar
+        musicToolBar = findViewById(R.id.toolbarMusicPlayer)
+        setSupportActionBar(musicToolBar)
     }
 }
